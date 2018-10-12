e952b55908e435ee148ef5d006bd7e9f8c3f8136
Ayana Bando <ayanaflax@yahoo.com>
2018-10-12 18:19:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,5 +44,9 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
   
-  
+    delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :delete
+  end	  end
 end
